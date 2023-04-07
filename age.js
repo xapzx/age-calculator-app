a0d3a47f4fb5161d0754004617de9b3e401830d5
Andy Phan <andyphan@live.com.au>
2023-04-07 15:04:03 +0930

    Set age on screen. Added screenshot

--- age.js
+++ age.js
@@ -4,6 +4,9 @@ const dayInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const day = document.querySelector('.day-input');
 const month = document.querySelector('.month-input');
 const year = document.querySelector('.year-input');
+const dayData = document.querySelector('.age-days-data');
+const monthData = document.querySelector('.age-months-data');
+const yearData = document.querySelector('.age-years-data');
 
 function setError(input, errorMsg) {
     input.classList.add('error');
@@ -45,14 +48,18 @@ function validateDay(day, month, year) {
         }
     } else {
         removeError(day);
+        return true;
     }
+    return false;
 }
 
 function validateMonth(month) {
     const thisMonth = month.children[1].value;
     if(thisMonth < 1 || thisMonth > 12) {
         setError(month, "Must be a valid month");
+        return false;
     }
+    return true;
 }
 
 function validateYear(year, month, day) {
@@ -63,11 +70,37 @@ function validateYear(year, month, day) {
     
     if(year.children[1].value > currentYear) {
         setError(year, "Must be in the past");
-    } else if(month.children[1].value > currentMonth) {
+    } else if(year.children[1].value > currentYear && month.children[1].value > currentMonth) {
         setError(month, "Must be in the past");
     } else if(month.children[1].value === currentMonth && day.children[1].value > currentDay) {
         setError(day, "Must be in the past");
+    } else {
+        return true;
     }
+    return false;
+}
+
+function setAge() {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() + 1;
+    const currentDay = today.getDate();
+
+    let ageYears = year.children[1].value - currentYear;
+    let ageMonths = month.children[1].value - currentMonth;
+    let ageDays = day.children[1].value - currentDay;
+    
+    let age = (Math.abs(ageYears) * 365) + (Math.abs(ageMonths) * 31) + Math.abs(ageDays);
+    ageYears = age / 365;
+    yearData.innerText = Math.round(ageYears);
+
+    let remainder = ageYears.toString().split('.')[1];
+    ageMonths = Number("0." + remainder) * 12;
+    monthData.innerText = Math.round(ageMonths);
+
+    remainder = ageMonths.toString().split('.')[1];
+    ageDays = Number("0." + remainder) * 31;
+    dayData.innerText = Math.round(ageDays);
 }
 
 calculate.addEventListener('click', () => {
@@ -75,8 +108,8 @@ calculate.addEventListener('click', () => {
         validateEmpty(element);
     });
 
-    validateDay(day, month, year);
-    validateMonth(month);
-    validateYear(year, month, day);
+    if(validateDay(day, month, year), validateMonth(month), validateYear(year, month, day)) {
+        setAge();
+    }
 });
 
